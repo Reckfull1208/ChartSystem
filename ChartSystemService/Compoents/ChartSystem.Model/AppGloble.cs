3bbc4f84061a8043b0fa63b37f3427ed35a6e581
reckfull <Reckfull1208@163.com>
2019-05-12 14:05:47 +0800

    更新

---
+++ ChartSystemService/Compoents/ChartSystem.Model/AppGloble.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartSystem.Model
+{
+    public class AppGloble
+    {
+        ///<summary>
+        ///依据连接串名字connectionName返回数据连接字符串
+        ///</summary>
+        ///<param name="connectionName">连接串名字</param>
+        ///<returns></returns>
+        public static string GetConnectionStringsConfig(string connectionName)
+        {
+            string connectionString =
+                ConfigurationManager.ConnectionStrings[connectionName].ConnectionString.ToString();
+            return connectionString;
+        }
+    }
+}
